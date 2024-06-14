049d9873eec69a4de3b68d4310d63e2d6110974c
Simon Murphy <simon.murphy@gmail.com>
2024-06-14 13:01:56 +0000

    Refined tests - improved parametrics

--- tests/test_parse_cards.py
+++ tests/test_parse_cards.py
@@ -12,3 +12,14 @@ from ..simpledeck.simpledeck import Card, parse_cards
 def test_single_cardt(test_input, rank, suit):
     assert parse_cards(test_input)[0].rank == rank
     assert parse_cards(test_input)[0].suit == suit
+
+
+@pytest.mark.parametrize("string_input, ranks, suits, length", [("4D,6S", [4,6],["Diamonds", "Spades"], 2),
+                                                        ("9C,AH", [9,14], ["Clubs", "Hearts"], 2),
+                                                        ("8C,9S,AD", [8, 9, 14], ["Clubs", "Spades", "Diamonds"], 3)])
+
+
+def test_two_cardt(string_input, ranks, suits, length):
+    assert len(parse_cards(string_input)) == length
+    assert all([a==b for a, b in zip((c.rank for c in parse_cards(string_input)), ranks)])
+    assert all([a==b for a, b in zip((c.suit for c in parse_cards(string_input)), suits)])
